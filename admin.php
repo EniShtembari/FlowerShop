54cfcf84e83d2211083747b14f39adab0a0984d5
Ajsela <ajseladuka196@gmail.com>
2025-01-05 21:11:29 +0100

    adm

--- admin.php
+++ admin.php
@@ -47,7 +47,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin - Add Product</title>
+    <title> Add Product</title>
     <style>
         body {
             font-family: Arial, sans-serif;
