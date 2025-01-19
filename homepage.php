edd7167f9707ba3d5c280b186bf5ef80730cb6db
EniShtembari <shtembarieni@gmail.com>
2025-01-19 15:07:03 +0100

    remove add to cart for homepage

--- homepage.php
+++ homepage.php
@@ -188,11 +188,6 @@ if (!isset($_SESSION['user_id'])) {
                 <!--text-->
                 <div class="product-box-text">
                     <a href="" class="product-text-title">Flower Pot</a>
-                    <span>20$ <del>40$</del></span>
-                    <a href="#" class="product-cart-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="1em"  width="1em"><path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64l0 48-128 0 0-48zm-48 48l-64 0c-26.5 0-48 21.5-48 48L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-208c0-26.5-21.5-48-48-48l-64 0 0-48C336 50.1 285.9 0 224 0S112 50.1 112 112l0 48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg>
-                        Add to Cart
-                    </a>
                 </div>
             </div>
 
@@ -205,11 +200,6 @@ if (!isset($_SESSION['user_id'])) {
                 <!--text-->
                 <div class="product-box-text">
                     <a href="" class="product-text-title">Flower Pot</a>
-                    <span>20$ <del>40$</del></span>
-                    <a href="#" class="product-cart-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="1em"  width="1em"><path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64l0 48-128 0 0-48zm-48 48l-64 0c-26.5 0-48 21.5-48 48L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-208c0-26.5-21.5-48-48-48l-64 0 0-48C336 50.1 285.9 0 224 0S112 50.1 112 112l0 48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg>
-                        Add to Cart
-                    </a>
                 </div>
             </div>
 
@@ -222,11 +212,6 @@ if (!isset($_SESSION['user_id'])) {
                 <!--text-->
                 <div class="product-box-text">
                     <a href="" class="product-text-title">Flower Pot</a>
-                    <span>20$ <del>40$</del></span>
-                    <a href="#" class="product-cart-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="1em"  width="1em"><path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64l0 48-128 0 0-48zm-48 48l-64 0c-26.5 0-48 21.5-48 48L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-208c0-26.5-21.5-48-48-48l-64 0 0-48C336 50.1 285.9 0 224 0S112 50.1 112 112l0 48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg>
-                        Add to Cart
-                    </a>
                 </div>
             </div>
 
@@ -239,11 +224,6 @@ if (!isset($_SESSION['user_id'])) {
                 <!--text-->
                 <div class="product-box-text">
                     <a href="" class="product-text-title">Flower Pot</a>
-                    <span>20$ <del>40$</del></span>
-                    <a href="#" class="product-cart-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="1em"  width="1em"><path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64l0 48-128 0 0-48zm-48 48l-64 0c-26.5 0-48 21.5-48 48L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-208c0-26.5-21.5-48-48-48l-64 0 0-48C336 50.1 285.9 0 224 0S112 50.1 112 112l0 48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg>
-                        Add to Cart
-                    </a>
                 </div>
             </div>
 
@@ -256,11 +236,6 @@ if (!isset($_SESSION['user_id'])) {
                 <!--text-->
                 <div class="product-box-text">
                     <a href="" class="product-text-title">Flower Pot</a>
-                    <span>20$ <del>40$</del></span>
-                    <a href="#" class="product-cart-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="1em"  width="1em"><path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64l0 48-128 0 0-48zm-48 48l-64 0c-26.5 0-48 21.5-48 48L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-208c0-26.5-21.5-48-48-48l-64 0 0-48C336 50.1 285.9 0 224 0S112 50.1 112 112l0 48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg>
-                        Add to Cart
-                    </a>
                 </div>
             </div>
 
@@ -273,11 +248,6 @@ if (!isset($_SESSION['user_id'])) {
                 <!--text-->
                 <div class="product-box-text">
                     <a href="" class="product-text-title">Flower Pot</a>
-                    <span>20$ <del>40$</del></span>
-                    <a href="#" class="product-cart-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="1em"  width="1em"><path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64l0 48-128 0 0-48zm-48 48l-64 0c-26.5 0-48 21.5-48 48L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-208c0-26.5-21.5-48-48-48l-64 0 0-48C336 50.1 285.9 0 224 0S112 50.1 112 112l0 48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg>
-                        Add to Cart
-                    </a>
                 </div>
             </div>
 
