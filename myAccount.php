c19ec0ec3a655647c6855b793bebaa7d7705e05e
EniShtembari <shtembarieni@gmail.com>
2025-01-20 22:52:19 +0100

    fix css

--- myAccount.php
+++ myAccount.php
@@ -98,7 +98,7 @@ include 'header.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="myAccount.css">
+    <!--<link rel="stylesheet" href="myAccount.css">-->
     <link rel="stylesheet" href="http://localhost/flowershop/myAccount.css">
     <title>My Account</title>
 
